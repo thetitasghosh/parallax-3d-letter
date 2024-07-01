b05654e1729399e4f494138e659296ee0b153749
Titas Ghosh <rg814602@gmail.com>
2024-07-01 10:43:11 +0530

    change herosection

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
-'use client'
+"use client";
 import { useEffect } from "react";
 import style from "./page.module.css";
 import dynamic from "next/dynamic";
 
-const Scene = dynamic(()=> import ('@/components/Scene'),{
-  ssr:false,
-})
-
-
+const Scene = dynamic(() => import("@/components/Scene"), {
+  ssr: false,
+});
 
 export default function Home() {
-
   // useEffect(()=>{
   //     const lenis = new Lenis()
 
@@ -22,10 +19,25 @@ export default function Home() {
   // },[])
   return (
     <main className={style.main}>
-     {/* <h1 className="text-white">Skull</h1> */}
-     <div className={style.page1} />
-     <Scene />
-     <div className={style.page2}/>
+      {/* <h1 className="text-white">Skull</h1> */}
+      <div className={style.page1}>
+        <div className="flex items-center justify-center flex-col font-bold ">
+          <h1>
+            {" "}
+            <em>t</em> itas <em>g</em>hosh
+          </h1>
+          <h1>creative front end</h1>
+          <h1>
+            <em>★</em> developer ⁂
+          </h1>
+        </div>
+        <div className="text-xl flex items-center justify-center flex-col">
+          <h1>Scroll Down</h1>
+          <h1 className="text-5xl">↓</h1>
+        </div>
+      </div>
+      <Scene />
+      <div className={style.page2} />
     </main>
   );
 }
